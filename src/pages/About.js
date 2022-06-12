ee33ab52e3baa114b38a628e8f0dba913d17e648
Gbenuola <golaiya@andrew.cmu.edu>
2022-06-12 19:03:00 -0400

    About Page Completed; Subtle bug in Home Page that I am yet to figure out.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import "./About.css";
+import "../assets/MMA.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              src="MMA.jpg"
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Gbenuola Ikenna Nehemiah Olaiya</div>
+            <div className="brief_description">
+              Hi, I'm Gbenuola! A CS Major @ Carnegie Mellon and an avid fan of soccer.
+              Excited for IBM Accelerate
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
